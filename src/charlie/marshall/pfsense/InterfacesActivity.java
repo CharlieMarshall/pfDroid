13cbc54aa184eb0b5fc21be18e3dbe5ab05f26c3
Charlie Marshall <charlie0440@gmail.com>
2013-04-26 00:46:20 +0100

    Make inroads into the Interface pages

--- src/charlie/marshall/pfsense/InterfacesActivity.java
+++ src/charlie/marshall/pfsense/InterfacesActivity.java
@@ -19,6 +19,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class InterfacesActivity extends CustomActivity
 {
@@ -26,6 +27,7 @@ public class InterfacesActivity extends CustomActivity
 	private SubDrop sd;
 	private int menu, subDrop;
 
+	// variables scraped from page
 	private boolean interfaceStatus = false;
 	private int selectedType, selectedSpeed;
 	private ArrayList<String> typeStore, speedStore; 
@@ -55,21 +57,33 @@ public class InterfacesActivity extends CustomActivity
 	 */
 
 	public void drawInterface()
-	{
-		CheckBox interf = (CheckBox) findViewById(R.id.enableInterface);
-		
-		Spinner typeSpinner = (Spinner) findViewById(R.id.type);  
-		Spinner speedSpinner = (Spinner) findViewById(R.id.speed);  
+	{				
 
-		EditText descET = (EditText) findViewById(R.id.desc);
-		EditText macET = (EditText) findViewById(R.id.macAddress);
-		EditText mtuET = (EditText) findViewById(R.id.mtu);
-		EditText mssET = (EditText) findViewById(R.id.mss);
-		
+		CheckBox interf = (CheckBox) findViewById(R.id.enableInterface);
+
 		if(interfaceStatus == true)
 		{
 			interf.setChecked(true); // tick the checkbox if interface is enabled
-			
+
+			// get reference to the general config elements
+			Spinner typeSpinner = (Spinner) findViewById(R.id.type);  			
+			Spinner speedSpinner = (Spinner) findViewById(R.id.speed);  
+
+			EditText descET = (EditText) findViewById(R.id.desc);
+			EditText macET = (EditText) findViewById(R.id.macAddress);
+			EditText mtuET = (EditText) findViewById(R.id.mtu);
+			EditText mssET = (EditText) findViewById(R.id.mss);
+
+			TextView descLabel = (TextView) findViewById(R.id.descLabel);
+			TextView typeLabel = (TextView) findViewById(R.id.typeLabel);
+			TextView macLabel = (TextView) findViewById(R.id.macLabel);
+			TextView mtuLabel = (TextView) findViewById(R.id.mtuLabel);
+			TextView mssLabel = (TextView) findViewById(R.id.mssLabel);
+			TextView speedLabel = (TextView) findViewById(R.id.speedLabel);
+
+			CheckBox privNet = (CheckBox) findViewById(R.id.privateNetworks);
+			CheckBox bogonNet = (CheckBox) findViewById(R.id.bogonNetworks);
+
 			// show the interface elements
 			typeSpinner.setVisibility(View.VISIBLE);
 			macET.setVisibility(View.VISIBLE);
@@ -77,26 +91,27 @@ public class InterfacesActivity extends CustomActivity
 			descET.setVisibility(View.VISIBLE);
 			mssET.setVisibility(View.VISIBLE);
 			speedSpinner.setVisibility(View.VISIBLE);
-			
-			TextView descLabel = (TextView) findViewById(R.id.descLabel);
 			descLabel.setVisibility(View.VISIBLE);
-			TextView typeLabel = (TextView) findViewById(R.id.typeLabel);
 			typeLabel.setVisibility(View.VISIBLE);
-			TextView macLabel = (TextView) findViewById(R.id.macLabel);
 			macLabel.setVisibility(View.VISIBLE);
-			TextView mtuLabel = (TextView) findViewById(R.id.mtuLabel);
 			mtuLabel.setVisibility(View.VISIBLE);
-			TextView mssLabel = (TextView) findViewById(R.id.mssLabel);
 			mssLabel.setVisibility(View.VISIBLE);
-			TextView speedLabel = (TextView) findViewById(R.id.speedLabel);
 			speedLabel.setVisibility(View.VISIBLE);
-			
-			// bind to the ArrayList (needs a toString method in the SectorList class)
-			ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, typeStore);
-			spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			typeSpinner.setAdapter(spinnerArrayAdapter);
-			typeSpinner.setSelection(selectedType);
+			privNet.setVisibility(View.VISIBLE);
+			bogonNet.setVisibility(View.VISIBLE);
 
+			// bind to the ArrayList
+			ArrayAdapter<String> typeArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, typeStore);
+			typeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			typeSpinner.setAdapter(typeArrayAdapter);
+			typeSpinner.setSelection(selectedType);
+			// bind to the ArrayList
+			ArrayAdapter<String> speedArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, speedStore);
+			speedArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			speedSpinner.setAdapter(speedArrayAdapter);
+			speedSpinner.setSelection(selectedType);
+
+			// fill the editText fields
 			descET.setText(description);
 			macET.setText(mac);
 			mtuET.setText(mtu);
@@ -105,18 +120,63 @@ public class InterfacesActivity extends CustomActivity
 			// bind to the ArrayList (needs a toString method in the SectorList class)
 			ArrayAdapter<String> spinnerSpeedArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, speedStore);
 			spinnerSpeedArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			speedSpinner.setAdapter(spinnerArrayAdapter);
+			speedSpinner.setAdapter(spinnerSpeedArrayAdapter);
 			speedSpinner.setSelection(selectedSpeed);
+
+			// TODO
+			// fill out if else statement 
+			
+			// display nothing if "None" was selected
+			//if(typeSpinner.getSelectedItem().toString().equals("None"))
+
+			if(typeSpinner.getSelectedItem().toString().trim().equals("Static"))
+			{
+				Toast.makeText(getApplicationContext(), "Static", Toast.LENGTH_SHORT).show();
+
+				TextView staticIpLabel = (TextView) findViewById(R.id.staticIpLabel);
+				EditText staticIp = (EditText) findViewById(R.id.staticIp);
+				Spinner staticSubnet = (Spinner) findViewById(R.id.staticSubnet);
+
+				staticIpLabel.setVisibility(View.VISIBLE);
+				staticIp.setVisibility(View.VISIBLE);
+				staticSubnet.setVisibility(View.VISIBLE);
+			}
+			else if(typeSpinner.getSelectedItem().toString().equals("DHCP"))
+				Toast.makeText(getApplicationContext(), "DHCP", Toast.LENGTH_SHORT).show();
+			else if(typeSpinner.getSelectedItem().toString().equals("PPP"))
+				Toast.makeText(getApplicationContext(), "PPP", Toast.LENGTH_SHORT).show();
+			else if(typeSpinner.getSelectedItem().toString().equals("PPPoE"))
+				Toast.makeText(getApplicationContext(), "PPPoE", Toast.LENGTH_SHORT).show();
+			else if(typeSpinner.getSelectedItem().toString().equals("PPTP"))
+				Toast.makeText(getApplicationContext(), "PPTP", Toast.LENGTH_SHORT).show();
+			else if(typeSpinner.getSelectedItem().toString().equals("L2TP"))
+				Toast.makeText(getApplicationContext(), "L2TP", Toast.LENGTH_SHORT).show();
+
+
 		}
 		else
-		{
-			// hide settings if interface is disabled
-			interf.setChecked(false);
-			
-		}
+			interf.setChecked(false); // if interface is disabled untick the checkbox
 
-		
+
 	}
+
+
+	/*
+	 * OnClick methods for the Save button
+	 */
+
+	// TODO once page if feature complete I need to implement pushing changes to pfSense
+	public void onClick(View view)
+	{	
+		switch (view.getId()) 
+		{
+		case R.id.save:
+			Log.d(TAG, "Need to update pfsense");
+			break;
+		}
+	}
+
+
 	/*
 	 * Subclass for ASYNC task
 	 * 
